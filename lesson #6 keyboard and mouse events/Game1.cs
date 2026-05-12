8bba913f7e2a7bf9d25d4751eefca2931f4b44ae
362707242 <362707242@2260I-R31-001>
2026-05-12 12:47:40 -0400

    Add project files.

---
+++ lesson #6 keyboard and mouse events/Game1.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace lesson__6_keyboard_and_mouse_events
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Texture2D pacTexture, pacUpTexture, pacDownTexture, pacRightTexture, pacLeftTexture, pacSleepTexture;
+        Rectangle pacLocation;
+        Vector2 pacSpeed;
+        KeyboardState keyboardState;
+        Rectangle window;
+
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            window = new Rectangle(0,0,800,600);
+            _graphics.PreferredBackBufferHeight = window.Height;
+            _graphics.PreferredBackBufferWidth = window.Width;
+            _graphics.ApplyChanges();
+            pacLocation = new Rectangle(10, 10, 75, 75);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            pacUpTexture = Content.Load<Texture2D>("PacUp");
+            pacDownTexture = Content.Load<Texture2D>("PacDown");
+            pacLeftTexture = Content.Load<Texture2D>("PacLeft");
+            pacRightTexture = Content.Load<Texture2D>("PacRight");
+            pacSleepTexture = Content.Load<Texture2D>("PacSleep");
+            pacTexture = pacSleepTexture;
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            keyboardState = Keyboard.GetState();
+            pacSpeed = new Vector2();
+            
+           
+            pacSpeed = Vector2.Zero;
+            if (keyboardState.IsKeyDown(Keys.Up))
+            {
+                pacTexture = pacUpTexture;
+                pacSpeed.Y -= 2;
+            }
+            if (keyboardState.IsKeyDown(Keys.Down))
+            {
+                pacTexture = pacDownTexture;
+                pacSpeed.Y += 2;
+            }
+            if (keyboardState.IsKeyDown(Keys.Right))
+            {
+                pacTexture = pacRightTexture;
+                pacSpeed.X += 2;
+            }
+            if (keyboardState.IsKeyDown(Keys.Left))
+            {
+                pacTexture = pacLeftTexture;
+                pacSpeed.X -= 2;
+            }
+            if (!keyboardState.IsKeyDown(Keys.Up) && !keyboardState.IsKeyDown(Keys.Right) && !keyboardState.IsKeyDown(Keys.Left) && !keyboardState.IsKeyDown(Keys.Down))
+            {
+                pacTexture = pacSleepTexture;
+            }
+            if (pacLocation.Bottom < 0)
+            {
+                pacLocation.Y = window.Height;
+            }
+            if (pacLocation.Bottom > window.Height + 76)
+            {
+                pacLocation.Y = -74;
+            }
+                pacLocation.X += (int)pacSpeed.X;
+            pacLocation.Y += (int)pacSpeed.Y;
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(pacTexture, pacLocation, Color.White);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ lesson #6 keyboard and mouse events/Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new lesson__6_keyboard_and_mouse_events.Game1();
+game.Run();
