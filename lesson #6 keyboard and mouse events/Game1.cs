a246b7cc1b6159536de18e9baaddf92578ae5239
362707242 <362707242@2260I-R31-001>
2026-05-14 12:17:23 -0400

    all done

--- lesson #6 keyboard and mouse events/Game1.cs
+++ lesson #6 keyboard and mouse events/Game1.cs
@@ -14,6 +14,9 @@ namespace lesson__6_keyboard_and_mouse_events
         Vector2 pacSpeed;
         KeyboardState keyboardState;
         Rectangle window;
+        MouseState mouseboardState;
+        int speed;
+        SpriteFont font;
 
 
         public Game1()
@@ -44,6 +47,7 @@ namespace lesson__6_keyboard_and_mouse_events
             pacRightTexture = Content.Load<Texture2D>("PacRight");
             pacSleepTexture = Content.Load<Texture2D>("PacSleep");
             pacTexture = pacSleepTexture;
+            font = Content.Load<SpriteFont>("info");
 
             // TODO: use this.Content to load your game content here
         }
@@ -55,28 +59,41 @@ namespace lesson__6_keyboard_and_mouse_events
 
             keyboardState = Keyboard.GetState();
             pacSpeed = new Vector2();
-            
-           
+            mouseboardState = Mouse.GetState();
+            speed = 2;
+
+            if (keyboardState.IsKeyDown(Keys.G))
+            {
+                speed -= 1;
+            }
+            if (keyboardState.IsKeyDown(Keys.H))
+            {
+                speed += 1;
+            }
+            if (speed <= 0)
+            {
+                speed = 1;
+            }
             pacSpeed = Vector2.Zero;
             if (keyboardState.IsKeyDown(Keys.Up))
             {
                 pacTexture = pacUpTexture;
-                pacSpeed.Y -= 2;
+                pacSpeed.Y -= speed;
             }
             if (keyboardState.IsKeyDown(Keys.Down))
             {
                 pacTexture = pacDownTexture;
-                pacSpeed.Y += 2;
+                pacSpeed.Y += speed;
             }
             if (keyboardState.IsKeyDown(Keys.Right))
             {
                 pacTexture = pacRightTexture;
-                pacSpeed.X += 2;
+                pacSpeed.X += speed;
             }
             if (keyboardState.IsKeyDown(Keys.Left))
             {
                 pacTexture = pacLeftTexture;
-                pacSpeed.X -= 2;
+                pacSpeed.X -= speed;
             }
             if (!keyboardState.IsKeyDown(Keys.Up) && !keyboardState.IsKeyDown(Keys.Right) && !keyboardState.IsKeyDown(Keys.Left) && !keyboardState.IsKeyDown(Keys.Down))
             {
@@ -90,7 +107,22 @@ namespace lesson__6_keyboard_and_mouse_events
             {
                 pacLocation.Y = -74;
             }
-                pacLocation.X += (int)pacSpeed.X;
+            if (pacLocation.Right < 0)
+            {
+                pacLocation.X = window.Width;
+            }
+            if (pacLocation.Right > window.Width + 76)
+            {
+                pacLocation.X = -74;
+            }
+            
+
+            if (mouseboardState.LeftButton == ButtonState.Pressed)
+            {
+                pacLocation.X = mouseboardState.X - 37;
+                pacLocation.Y = mouseboardState.Y - 37;
+            }
+            pacLocation.X += (int)pacSpeed.X;
             pacLocation.Y += (int)pacSpeed.Y;
 
             base.Update(gameTime);
@@ -103,7 +135,8 @@ namespace lesson__6_keyboard_and_mouse_events
             _spriteBatch.Begin();
 
             _spriteBatch.Draw(pacTexture, pacLocation, Color.White);
-
+            _spriteBatch.DrawString(font, "Hold 'G' to go slower and 'H' to speed up", new Vector2(5,5), Color.White);
+            _spriteBatch.DrawString(font, "Arrow keys to move and click to teleport", new Vector2(5, 23), Color.White);
             _spriteBatch.End();
 
             base.Draw(gameTime);
